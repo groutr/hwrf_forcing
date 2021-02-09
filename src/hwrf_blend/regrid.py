9abab39d300d7a5279179ab13b56ea64d5f9262b
Ryan Grout <ryan.grout@noaa.gov>
2021-02-09 21:12:47 +0000

    Complete inital regridding implementation

--- src/hwrf_blend/regrid.py
+++ src/hwrf_blend/regrid.py
@@ -1,10 +1,11 @@
 import argparse
 import pathlib
+import time
 
 import ESMF
 import xarray as xr
 import numpy as np
-
+import netCDF4
 
 def make_lat_lon_grid(lat, lon):
     if lat.dtype == np.float32 or lon.dtype == np.float32:
@@ -12,7 +13,14 @@ def make_lat_lon_grid(lat, lon):
     else:
         typekind = ESMF.TypeKind.R8
 
-    G = ESMF.Grid(np.array(lat.shape),
+    if lat.ndim == 1 and lon.ndim == 1:
+        shape = np.array([lat.size, lon.size])
+        lat = np.atleast_2d(lat).T
+    else:
+        shape = np.array(lat.shape)
+
+    print("Creating grid of shape", shape)
+    G = ESMF.Grid(shape,
             coord_sys=ESMF.CoordSys.SPH_DEG,
             coord_typekind=typekind,
             staggerloc=ESMF.StaggerLoc.CENTER,
@@ -44,6 +52,9 @@ def get_options():
     parser.add_argument('forcings', type=pathlib.Path, help='Location of forcings file')
     parser.add_argument('nwm_grid', type=pathlib.Path, help='Location of NWM grid')
 
+    methods = tuple(ESMF.api.constants.RegridMethod.__members__.keys())
+    parser.add_argument('--method', choices=methods, help='Regridding method')
+    parser.add_argument('--filename', help='filename for ESMF weights cache')
     args = parser.parse_args()
     return args
 
@@ -52,18 +63,52 @@ def main(args):
     dst_grid = make_lat_lon_grid(*nwm_grid)
 
     with xr.open_dataset(args.forcings) as forcings:
-        flats = forcings['latitude'][:]
-        flons = forcings['longitude'][:]
+        flats = forcings['latitude'][:].values
+        flons = forcings['longitude'][:].values
         src_grid = make_lat_lon_grid(flats, flons)
 
+        print("Creating fields")
         srcF, dstF = make_fields(src_grid, dst_grid)
 
+        print("Creating ESMF regridding object")
+        filename = args.forcings.with_suffix('.esmf_weights')
+        rh_filename = filename.with_suffix('.routehandle')
+        start = time.perf_counter()
         regridder = ESMF.Regrid(srcF, dstF, src_mask_values=np.array([0]),
                 regrid_method=ESMF.RegridMethod.BILINEAR, unmapped_action=ESMF.UnmappedAction.IGNORE,
-                filename=filename, rh_filename=filename+'.routehandle')
+                rh_filename=str(rh_filename))
+        print("Regridding took:", time.perf_counter()-start)
 
+        clon = dstF.grid.get_coords(0)
+        clat = dstF.grid.get_coords(1)
+        rf = netCDF4.Dataset(str(filename.with_suffix(".regridded.nc")), mode='w')
+        rf.createDimension('time', len(forcings.time))
+        v = rf.createVariable('time', 'f8', dimensions=('time',), zlib=True)
+        v[:] = forcings.time.values
+        v.setncatts(forcings.time.attrs)
+        rf.createDimension('x', clon.shape[0])
+        rf.createDimension('y', clon.shape[1])
+        v = rf.createVariable('latitude', 'f8', dimensions=('x', 'y'), zlib=True)
+        v[:] = clat
+        v = rf.createVariable('longitude', 'f8', dimensions=('x', 'y'), zlib=True)
+        v[:] = clon
+
+        tmp = np.full((len(forcings.time), *clon.shape), np.nan, dtype='float32')
+        for V in forcings.data_vars:
+            print("Regridding", V)
+            fv = forcings[V]
+            rv = rf.createVariable(V, 'f4', dimensions=('time', 'x', 'y'), zlib=True)
+            tmp.fill(np.nan)
+            for ts in range(forcings.time.size):
+                print("timestep", ts, end='\r')
+                srcF.data[:] = fv[ts].values
+                regridder(srcF, dstF)
+                tmp[ts, ...] = dstF.data[:]
+            rv[:] = tmp
+        rf.close()
 
 
 if __name__ == "__main__":
     args = get_options()
+    main(args)
 
